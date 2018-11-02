5ff16c32e07cb4ceb5594923532e2d79a77a6d33
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2018-11-02 17:56:18 +0100

    More buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,39 @@
-import * as React from 'react';
+import * as React from "react";
 
-import { JSONSchema6 } from 'json-schema';
+import { JSONSchema6 } from "json-schema";
+import { Button, ButtonGroup, Glyphicon } from 'react-bootstrap';
 import Form from "react-jsonschema-form";
 
-import './App.css';
+import "./App.css";
 
 interface IState {
-  schema: JSONSchema6
+  schema: JSONSchema6;
 }
 
 class App extends React.Component<{}, IState> {
-  public state = { schema: {}};
+  public state = { schema: {} };
 
   public componentDidMount() {
-    fetch('schema.json').then(r => r.json()).then(schema => this.setState({schema}));
+    fetch("schema.json")
+      .then(r => r.json())
+      .then(schema => this.setState({ schema }));
   }
 
   public render() {
     return (
       <div className="App">
-        {this.state.schema ? <Form schema={this.state.schema}/> : <div/>}
+        {this.state.schema ? (
+          <Form schema={this.state.schema}>
+            <ButtonGroup>
+              <Button><Glyphicon glyph="upload" /> Upload</Button>
+              <Button><Glyphicon glyph="download" /> Download</Button>
+              <Button bsStyle="primary"><Glyphicon glyph="ok" /> Save</Button>
+              <Button><Glyphicon glyph="remove" /> Reset</Button>
+            </ButtonGroup>
+          </Form>
+        ) : (
+          <div />
+        )}
       </div>
     );
   }
