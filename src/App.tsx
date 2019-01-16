9e60221a37eb2cf1f5c9f010d1f5ae85e6bffc9d
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2019-01-16 17:00:01 +0100

    Use async TypeAhead field
    Does not work as expected, as once selection is made you are unable to change it.
    Except by removing the whole row and starting over.

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { JSONSchema6 } from "json-schema";
 import { Button, ButtonGroup, Glyphicon } from 'react-bootstrap';
 import Form, { ISubmitEvent } from "react-jsonschema-form";
 import CollapsibleField from "react-jsonschema-form-extras/lib/CollapsibleField";
-
+import { AsyncTypeaheadField } from "react-jsonschema-form-extras/lib/TypeaheadField";
 
 import "./App.css";
 
@@ -15,11 +15,13 @@ interface IState {
 }
 
 const formFields = {
-  collapsible: CollapsibleField
+  collapsible: CollapsibleField,
+  asyncTypeahead: AsyncTypeaheadField
 };
 
 class App extends React.Component<{}, IState> {
   public state: IState = { schema: {}, uiSchema: {}, formData: {} };
+  public rawFormData: any = {};
 
   public componentDidMount() {
     fetch("schema.json")
@@ -28,12 +30,72 @@ class App extends React.Component<{}, IState> {
     fetch("uischema.json")
       .then(r => r.json())
       .then(uiSchema => {
+        uiSchema.Genome_Metabolome_links.items.Genome_Metagenome_ID.asyncTypeahead.search = this.searchLabels.bind(this);
+        uiSchema.Genome_Metabolome_links.items.Sample_preparation_label.asyncTypeahead.search = this.searchLabels.bind(this);
+        uiSchema.Genome_Metabolome_links.items.Extraction_method_label.asyncTypeahead.search = this.searchLabels.bind(this);
+        uiSchema.Genome_Metabolome_links.items.Instrumentation_method_label.asyncTypeahead.search = this.searchLabels.bind(this);
         this.setState({ uiSchema });
       });
   }
 
+  public searchLabels = (url: string, query: string) => {
+    if (Object.keys(this.rawFormData.data_to_link).length === 0) {
+      return Promise.reject(new Error('Missing data to link'));
+    }
+    if (url === 'Genome_Metagenome_ID') {
+      if (this.rawFormData.data_to_link['metagenome_genome sequence_assemblies'] === undefined) {
+        return Promise.resolve([]);
+      }
+
+      let labels = this.rawFormData.data_to_link['metagenome_genome sequence_assemblies'].map(
+        (r: any) => r.GenBank_accession || r.RefSeq_accession || r.ENA_NCBI_accession || r.MGnify_accession
+      );
+      if (query) {
+        labels = labels.filter((label: string) => label.startsWith(query));
+      }
+      return Promise.resolve(labels);
+    } else if (url === 'Sample_preparation_Method') {
+      if (this.rawFormData.data_to_link.Experimental_details.Sample_Preparation === undefined) {
+        return Promise.resolve([]);
+      }
+
+      let labels = this.rawFormData.data_to_link.Experimental_details.Sample_Preparation.map(
+        (r: any) => r.Sample_preparation_Method
+      );
+      if (query) {
+        labels = labels.filter((label: string) => label.startsWith(query));
+      }
+      return Promise.resolve(labels);
+    } else if (url === 'Extraction_method_label') {
+      if (this.rawFormData.data_to_link.Experimental_details['Extraction Methods'] === undefined) {
+        return Promise.resolve([]);
+      }
+
+      let labels = this.rawFormData.data_to_link.Experimental_details['Extraction Methods'].map((r: any) => r.Extraction_Method);
+      if (query) {
+        labels = labels.filter((label: string) => label.startsWith(query));
+      }
+      return Promise.resolve(labels);
+    } else if (url === 'Instrumentation_method_label') {
+      if (this.rawFormData.data_to_link.Experimental_details['Instrumentation Methods'] === undefined) {
+        return Promise.resolve([]);
+      }
+
+      let labels = this.rawFormData.data_to_link.Experimental_details['Instrumentation Methods'].map((r: any) => r.Instrumentation_Method);
+      if (query) {
+        labels = labels.filter((label: string) => label.startsWith(query));
+      }
+      return Promise.resolve(labels);
+    }
+    return Promise.reject(new Error('Unknown link'));
+  }
+
   public onSubmit = ({ formData }: ISubmitEvent<object>) => {
     this.setState({ formData });
+  }
+
+  public onFormChange = ({ formData }: ISubmitEvent<object>) => {
+    this.rawFormData = formData;
   }
 
   public render() {
@@ -46,6 +108,7 @@ class App extends React.Component<{}, IState> {
             fields={formFields}
             formData={this.state.formData}
             onSubmit={this.onSubmit}
+            onChange={this.onFormChange}
             liveValidate={true}
           >
             <ButtonGroup>
--- src/react-jsonschema-form-extras.d.ts
+++ src/react-jsonschema-form-extras.d.ts
@@ -1,2 +1,3 @@
 declare module 'react-jsonschema-form-extras/lib/CollapsibleField';
 declare module 'react-jsonschema-form-extras/lib/table';
+declare module 'react-jsonschema-form-extras/lib/TypeaheadField';
