b2ea5e29277fd043bde5ce22624b8b8b4e9fc22f
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2019-02-01 16:47:53 +0100

    Force options to be recomputed when select is pulled down

--- src/ForeignKeyField.tsx
+++ src/ForeignKeyField.tsx
@@ -1,35 +1,71 @@
 import * as React from "react";
 
-import { FieldProps } from 'react-jsonschema-form';
-import AsyncSelect from 'react-select/lib/Async';
+import { Glyphicon } from 'react-bootstrap';
+import { FieldProps } from "react-jsonschema-form";
+import Select from 'react-select'
+
 
 export class ForeignKeyField extends React.Component<FieldProps, {}> {
-    public handleChange = (option: any) => {
-        this.props.onChange(option.value);
-    }
+  public state = {
+    open: false,
+    options: []
+  };
 
-    public loadOptions = () => {
-        const values = this.props.uiSchema.foreignKey.search(this.props.name);
-        return values.then((vals: string[]) => {
-            return vals.map((v: string) => {
-                return { value: v, label: v };
-            });
-        });
-    }
+  public handleChange = (option: any) => {
+    this.props.onChange(option.value);
+  };
 
-    public render() {
-        return (
-            <div>
-                <label>{this.props.schema.title}</label>
-                <p>{this.props.schema.description}</p>
-                <AsyncSelect
-                    defaultValue={this.props.formData}
-                    cacheOptions={false}
-                    defaultOptions={true}
-                    loadOptions={this.loadOptions}
-                    onChange={this.handleChange}
-                />
-            </div>
-        );
+  public loadOptions = () => {
+    const values = this.props.uiSchema.foreignKey.search(this.props.name);
+    return values.then((vals: string[]) => {
+      return vals.map((v: string) => {
+        return { value: v, label: v };
+      });
+    });
+  };
+
+  public render() {
+    let field = (
+      <span className="form-control" onClick={this.onOpen}>
+        {this.props.formData || "Click to select"}
+        <span className="pull-right"><Glyphicon glyph="chevron-down"/></span>
+      </span>
+    );
+    if (this.state.open && this.state.options.length > 0) {
+      field = (
+        <Select
+          defaultValue={this.props.formData}
+          onChange={this.onClose}
+          options={this.state.options}
+          menuIsOpen={true}
+        />
+      );
     }
+    return (
+      <div>
+        <label>{this.props.schema.title}</label>
+        <p>{this.props.schema.description}</p>
+        {field}
+      </div>
+    );
+  }
+
+  public onOpen = async () => {
+    const options = await this.loadOptions();
+    this.setState({ open: true, options });
+  };
+
+  public onClose = (event: any) => {
+    const value = event.value;
+    this.setState(
+      {
+        value,
+        open: false,
+        options: []
+      },
+      () => {
+        this.props.onChange(value);
+      }
+    );
+  };
 }
