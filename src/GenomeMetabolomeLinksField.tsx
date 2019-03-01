f981d93649a5312127d11d66d5d761a69bdd9dad
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2019-03-01 18:08:12 +0100

    Added missing files

---
+++ src/GenomeMetabolomeLinksField.tsx
@@ -0,0 +1,47 @@
+import * as React from "react";
+
+import { tsvParse } from 'd3-dsv';
+import { Button, Glyphicon } from 'react-bootstrap';
+import { FieldProps } from 'react-jsonschema-form';
+import ArrayField from 'react-jsonschema-form/lib/components/fields/ArrayField';
+
+export const GenomeMetabolomeLinksField = (props: FieldProps) => {
+    const uploadRef = React.useRef<HTMLInputElement>(null);
+
+    function onClick() {
+        if (uploadRef.current) {
+            uploadRef.current.click();
+        }
+    }
+
+    function fillLinksFromFile(event: React.ChangeEvent<HTMLInputElement>) {
+        if (!event.target.files) {
+            return;
+          }
+          const file = event.target.files[0];
+          const reader = new FileReader();
+          reader.onload = () => {
+            if (reader.result) {
+              const rows = tsvParse(reader.result as string);
+              props.formContext.uploadGenomeMetabolomeLinks(rows);
+            }
+          };
+          reader.readAsText(file);
+    }
+
+    return (
+        <div>
+            <ArrayField {...props}/>
+            <Button onClick={onClick} title="Upload links from tab delimited file, will replace existing links, (meta)genomes and metabolomics experimental details">
+                <Glyphicon glyph="upload"/> Upload links
+                <input
+                    type="file"
+                    accept="text/tab-separated-values,.tsv,.txt"
+                    onChange={fillLinksFromFile}
+                    ref={uploadRef}
+                    style={{ display: "none" }}
+                />
+            </Button>
+        </div>
+    );
+}
