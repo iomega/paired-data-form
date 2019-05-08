daf4f7d05cf301cb6567362b71e8fe9754a9e425
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2019-05-08 14:44:17 +0200

    Dont log to files
    With Docker all logs are outputted to stderr/stdout

--- api/src/util/logger.ts
+++ api/src/util/logger.ts
@@ -4,8 +4,7 @@ import { ENVIRONMENT } from './secrets';
 
 const logger = new (Logger)({
     transports: [
-        new (winston.transports.Console)({ level: process.env.NODE_ENV === 'production' ? 'error' : 'debug' }),
-        new (winston.transports.File)({ filename: 'debug.log', level: 'debug'})
+        new (winston.transports.Console)({ level: process.env.NODE_ENV === 'production' ? 'error' : 'debug' })
     ]
 });
 
