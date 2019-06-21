6ded18a3673bd1426aedb2f0474d0f7071f3db92
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2019-06-21 15:20:11 +0200

    Added overview image to welcome page

--- app/src/pages/Welcome.tsx
+++ app/src/pages/Welcome.tsx
@@ -1,11 +1,15 @@
 import * as React from "react";
+import overview from './overview.png';
+
+const style = {padding: '10px'};
 
 export function Welcome() {
     return (
-        <div>
+        <div style={style}>
             <h1>Paired omics data platform</h1>
             <p>This Paired Data Platform is a community-based initiative standardizing links between genomic and metabolomics data in a computer-readable manner to further the field of natural products discovery. 
             </p>
+            <img src={overview} width="100%" alt="Overview"/>
         </div>
     );
 }
