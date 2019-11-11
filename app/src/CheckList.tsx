37102d8049ee8f023f6222123d4a7049f95f343d
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2019-11-11 16:23:41 +0100

    Update checklist + welcome page
    Added images and text to welcome page
    
    Added Roboto font for welcome page text
    
    Refs #55

--- app/src/CheckList.tsx
+++ app/src/CheckList.tsx
@@ -2,23 +2,30 @@ import React from 'react';
 
 export const CheckList = () => (
     <div>
-        <p>Checklist before starting your submission:</p>
+        <p>It is great that you want to contribute to the Paired omics Data Platform! Please use provided checklist of what you will need at hand before starting your submission:</p>
         <ul>
-            <li>Deposit metabolomics data in MASSive or MetaboLights and make the data publically available</li>
-            <li>Deposit assembled genomes/metagenomes to a public database (NCBI, JGI, MGnify) and make them publically available</li>
+            <li>Deposit your metabolomics data in MASSive or MetaboLights and make the data publically available</li>
+            <li>Deposit your assembled genomes/metagenomes in a public database (GenBank, ENA, DDBJ, IMG-JGI or MGnify) and make them publically available</li>
             <li>Have the basic methods used available for reference</li>
             <li>Locate the URLs for each metabolomics sample in the repository</li>
         </ul>
-        <p>Only if connecting a BGC to molecule/molecular family:</p>
+        <p>Only if connecting one or more biosynthetic gene clusters to molecules/molecular families:</p>
         <ul>
-            <li>Deposit the cluster in MIBiG and have the BGC ID number available</li>
-            <li>Have the SMILES (textual representation of the structure) available</li>
-            <li>Locate the MS2 scan or molecular family ID</li>
+            <li>
+                Deposit the gene cluster in MIBiG and have the accession number available</li>
+            <li>
+                Have the SMILES (textual representation of the chemical structure) of the metabolites available</li>
+            <li>
+                Locate the MS2 scan or molecular family ID in GNPS</li>
         </ul>
         <p>Pro Tips:</p>
         <ul>
             <li>Load the sample data to get an idea of how to fill out the form.</li>
-            <li>To save your work, complete section 2 for at least one sample and make one link, then press the Preview button, scroll down to the table created and press the Download button. You can later Upload this file to continue working. All validation errors must be resolved before you can download the file.</li>
+            <li>
+                Fill out the form in order from section 1 to 6.</li>
+            <li>
+                To save your work, complete sections 1-4 for at least one sample and make at least one link in section 5, then press the Preview button, scroll down to the table created and press the Download button. You can later use the Upload button to upload this file to continue working. Validation errors may appear at the top of the page and must be resolved before you can download the file.
+        </li>
         </ul>
     </div>
 );
--- app/src/pages/Welcome.tsx
+++ app/src/pages/Welcome.tsx
@@ -1,15 +1,76 @@
 import * as React from "react";
-import overview from './overview.png';
 
-const style = {padding: '10px'};
+import { Row, Col } from "react-bootstrap";
+
+import slide1 from './welcome/slide1.png';
+import slide2 from './welcome/slide2.png';
+import slide3 from './welcome/slide3.png';
+import slide4 from './welcome/slide4.png';
+import slide5 from './welcome/slide5.png';
+
+const style = { padding: '10px', fontFamily: 'Roboto Condensed' };
+const textStyle = { fontSize: '1.3em' };
+const imgStyle = { width: '100%' };
+const rowStyle = { marginBottom: '50px' };
+const colStyle = { 
+    paddingLeft: '30px'
+};
 
 export function Welcome() {
     return (
         <div style={style}>
-            <h1>Paired omics data platform</h1>
-            <p>This Paired Data Platform is a community-based initiative standardizing links between genomic and metabolomics data in a computer-readable manner to further the field of natural products discovery. 
-            </p>
-            <img src={overview} width="100%" alt="Overview"/>
+            <Row style={rowStyle}>
+                <Col md={3} mdOffset={3} style={colStyle}>
+                    <h2>Pairing Omics Data</h2>
+                    <p style={textStyle}>The Paired Data Platform is a community-based initiative standardizing links between genomic and metabolomics data in a computer readable format to further the field of natural products discovery. The goals are to link molecules to their producers, find large scale genome-metabolome associations, use genomic data to assist in structural elucidation of molecules, and provide a centralized database for paired datasets.</p>
+                </Col>
+                <Col md={3} style={colStyle}>
+                    <img style={imgStyle} src={slide1} alt="Paired Omics Data" />
+                </Col>
+            </Row>
+            <Row style={rowStyle}>
+                <Col md={3} mdOffset={3} style={colStyle}>
+                    <img style={imgStyle} src={slide2} alt="FAIR Data" />
+                </Col>
+                <Col md={3} style={colStyle}>
+                    <h2>FAIR Data</h2>
+                    <p style={textStyle}>The Paired Data Platform relies on publically available data deposited in standard databases and promotes FAIR principles. Metabolomics project information stored in MASSive or MetaboLights can be quickly linked to public genomes stored in NCBI or JGI. Coupling these data with minimal experimental details in a computer readable format allows for large-scale correlations of metabolome changes. Linking these publically available data will facilitate new discoveries and algorithms for predicting chemical structures from genomic information.
+                </p>
+                </Col>
+            </Row>
+            <Row style={rowStyle}>
+                <Col md={3} mdOffset={3} style={colStyle}>
+                    <h2>Minimal Metadata</h2>
+                    <p style={textStyle}>
+                        The Paired Data Platform collects a set of minimal metadata in a standardized, computer readable format. Relevant experimental details related to Sample Preparation, Extraction Methods, and Instrumentation Methods can be added and identified with user-defined labels for quick recall and linking.
+                    </p>
+                </Col>
+                <Col md={3} style={colStyle}>
+                    <img style={imgStyle} src={slide3} alt="Minimal Metadata" />
+                </Col>
+            </Row>
+            <Row style={rowStyle}>
+                <Col md={3} mdOffset={3} style={colStyle}>
+                    <img style={imgStyle} src={slide4} alt="Quick Linking" />
+                </Col>
+                <Col md={3} style={colStyle}>
+                    <h2>Quick Linking</h2>
+                    <p style={textStyle}>The Paired Data Platform makes linking a mass spectrometry file with a genome and the associated metadata quick and easy. The modular format reduces repetition and allows for rapid input of large studies.
+                </p>
+                </Col>
+            </Row>
+            <Row style={rowStyle}>
+                <Col md={3} mdOffset={3} style={colStyle}>
+                    <h2>Gene Cluster – Molecule Linking
+                </h2>
+                    <p style={textStyle}>
+                        The Paired Data Platform also collects publically available links between known biosynthetic gene clusters and molecules or molecular families. These fine-grained connections will inform future large scale predictions of structures from genomic data.
+                </p>
+                </Col>
+                <Col md={3} style={colStyle}>
+                    <img style={{...imgStyle, paddingTop: '30px'}} src={slide5} alt="Gene Cluster – Molecule Linking" />
+                </Col>
+            </Row>
         </div>
     );
 }
