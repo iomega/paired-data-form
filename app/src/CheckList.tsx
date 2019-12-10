5e4cdb98018f5f260fb62c3bc6b0228fd0e990ba
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2019-12-10 10:31:30 +0100

    Added links to relevant sites + added resins field + added erlenmeyer flask to aeration vessel enum + space in url warning
    Refs #76
    Refs #75

--- app/src/CheckList.tsx
+++ app/src/CheckList.tsx
@@ -4,19 +4,19 @@ export const CheckList = () => (
     <div>
         <p>It is great that you want to contribute to the Paired omics Data Platform! Please use provided checklist of what you will need at hand before starting your submission:</p>
         <ul>
-            <li>Deposit your metabolomics data in MASSive or MetaboLights and make the data publically available</li>
-            <li>Deposit your assembled genomes/metagenomes in a public database (GenBank, ENA, DDBJ, IMG-JGI or MGnify) and make them publically available</li>
+            <li>Deposit your metabolomics data in <a target="_blank" href="https://gnps.ucsd.edu/">MASSive</a> or <a target="_blank" href="https://www.ebi.ac.uk/metabolights/">MetaboLights</a> and make the data publically available</li>
+            <li>Deposit your assembled genomes/metagenomes in a public database (<a target="_blank" href="https://www.ncbi.nlm.nih.gov/genbank/">GenBank</a>, <a target="_blank" title="European Nucleotide Archive" href="https://www.ebi.ac.uk/ena">ENA</a>, <a target="_blank" title="DNA Data Bank of Japan" href="https://www.ddbj.nig.ac.jp/ddbj/index-e.html">DDBJ</a>, <a target="_blank" title="Integrated Microbial Genomes & Microbiomes - Joint Genome Institute" href="https://img.jgi.doe.gov/">IMG-JGI</a> or <a target="_blank" title="EBI Metagenomics" href="https://www.ebi.ac.uk/metagenomics/">MGnify</a>) and make them publically available</li>
             <li>Have the basic methods used available for reference</li>
             <li>Locate the URLs for each metabolomics sample in the repository</li>
         </ul>
         <p>Only if connecting one or more biosynthetic gene clusters to molecules/molecular families:</p>
         <ul>
             <li>
-                Deposit the gene cluster in MIBiG and have the accession number available</li>
+                Deposit the gene cluster in <a target="_blank" href="https://mibig.secondarymetabolites.org/">MIBiG</a> and have the accession number available</li>
             <li>
                 Have the SMILES (textual representation of the chemical structure) of the metabolites available</li>
             <li>
-                Locate the MS2 scan or molecular family ID in GNPS</li>
+                Locate the MS2 scan or molecular family ID in <a target="_blank" href="https://gnps.ucsd.edu/">GNPS</a></li>
         </ul>
         <p>Pro Tips:</p>
         <ul>
