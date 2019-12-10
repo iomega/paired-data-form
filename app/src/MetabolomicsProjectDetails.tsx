05453996c07147132d975587b9577d78c16086e9
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2019-12-10 13:43:14 +0100

    Correct hrefs in metabolomics details section on project page
    Fixes #81

--- app/src/MetabolomicsProjectDetails.tsx
+++ app/src/MetabolomicsProjectDetails.tsx
@@ -13,7 +13,7 @@ export const MetabolomicsProjectDetails = ({ data }: IProps) => {
   if (isMetaboLights(project)) {
     return (
       <ul>
-        <li>Metabolights study identifier: <a href="https://www.ebi.ac.uk/metabolights/{repo.metabolights_study_id}">{project.metabolights_study_id}</a></li>
+        <li>Metabolights study identifier: <a href={`https://www.ebi.ac.uk/metabolights/${project.metabolights_study_id}`}>{project.metabolights_study_id}</a></li>
         <li>Publications: <Publications publications={data.publications!}/></li>
       </ul>
     );
@@ -21,7 +21,7 @@ export const MetabolomicsProjectDetails = ({ data }: IProps) => {
   return (
     <ul>
       <li>GNPS-MassIVE identifier: <a href={project.MaSSIVE_URL}>{project.GNPSMassIVE_ID}</a></li>
-      <li>Molecular Network Task ID: <a href="http://gnps.ucsd.edu/ProteoSAFe/status.jsp?task={project.molecular_network}">{project.molecular_network}</a></li>
+      <li>Molecular Network Task ID: <a href={`http://gnps.ucsd.edu/ProteoSAFe/status.jsp?task=${project.molecular_network}`}>{project.molecular_network}</a></li>
       <li>Publications: <Publications publications={data.publications!}/></li>
     </ul>
   );
