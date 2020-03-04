6795379a2bb2b5ab0c762e0846f11a5423574c44
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2020-03-04 08:12:16 +0100

    Use url for download

--- app/src/ProjectActions.tsx
+++ app/src/ProjectActions.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 
 import { Button, Glyphicon, ButtonGroup } from "react-bootstrap";
 import { Link} from "react-router-dom";
+import { getProjectJSONUrl } from "./api";
 
 interface IProps {
     data_url: string;
@@ -11,9 +12,14 @@ interface IProps {
 }
 
 export const ProjectActions = ({data_url, filename, project_id, inreview}: IProps) => {
+    const project_url = getProjectJSONUrl(project_id);
     return (
         <ButtonGroup style={{marginBottom: '20px'}}>
-            <Button href={data_url} download={filename} ><Glyphicon glyph="download" /> Download</Button>
+            { inreview ?
+                <Button href={data_url} download={filename} ><Glyphicon glyph="download" /> Download</Button>
+                :
+                <Button href={project_url} download={filename} ><Glyphicon glyph="download" /> Download</Button>
+            }
             <Link title="View history of project" className="btn btn-default" to={`/projects/${project_id}/history`}><Glyphicon glyph="sort" /> History</Link>
             { !inreview &&
                 <>
