9295bf80aadcb571bd29dcc188b94a01688e6d7a
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2020-03-06 16:33:14 +0100

    Render MiBIG accession correctly

--- app/src/GeneSpectraTable.test.tsx
+++ app/src/GeneSpectraTable.test.tsx
@@ -5,18 +5,51 @@ import { shallow } from "enzyme";
 import { kitchenSinkEnrichedDoc } from './test.fixtures';
 import { GeneSpectraTable } from "./GeneSpectraTable";
 
-describe('GenomeMetabolomicsTable', ()=> {
-    describe('with schema loaded and kitchen sick sample document rendered', () => {
+describe('<GenomeMetabolomicsTable/>', () => {
+    describe('with schema loaded', () => {
         let schema: any;
         let comp: any;
         beforeEach(() => {
             schema = require('../public/schema.json');
-            comp = shallow(<GeneSpectraTable schema={schema} data={kitchenSinkEnrichedDoc}/>);
         });
 
-        it('should show warning if there are no links', () => {
-            const warning = <p>No links between gene clusters and MS2 spectra.</p>;
-            expect(comp.contains(warning)).toBeTruthy();
+        describe('with kitchen sick sample document', () => {
+            beforeEach(() => {
+                comp = shallow(<GeneSpectraTable schema={schema} data={kitchenSinkEnrichedDoc} />);
+            });
+
+            it('should show warning if there are no links', () => {
+                const warning = <p>No links between gene clusters and MS2 spectra.</p>;
+                expect(comp.contains(warning)).toBeTruthy();
+            });
+        });
+
+        describe('with example document', () => {
+            beforeAll(() => {
+                const project = require('../public/examples/paired_datarecord_MSV000078839_example.json');
+                const eproject = {
+                    _id: 'someprojectid',
+                    project
+                }
+                comp = shallow(<GeneSpectraTable schema={schema} data={eproject} />);
+            });
+
+            it('should not show warning if there are no links', () => {
+                const warning = <p>No links between gene clusters and MS2 spectra.</p>;
+                expect(comp.contains(warning)).toBeFalsy();
+            });
+
+            it('should contain BGC0000827', () => {
+                expect(comp.html()).toContain('BGC0000827');
+            });
+
+            it('should contain "molecule of MS2 scan 977106"', () => {
+                expect(comp.html()).toContain('molecule of MS2 scan 977106');
+            })
+
+            it('should contain "GNPS molecular family"', () => {
+                expect(comp.html()).toContain('GNPS molecular family');
+            })
         });
     });
 });
