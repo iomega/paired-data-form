261578efc92a30198b8ee70cb3e0d33d909cb74c
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2020-04-23 08:37:25 +0200

    Added tests for pager

--- app/src/ProjectPager.tsx
+++ app/src/ProjectPager.tsx
@@ -12,15 +12,27 @@ interface Props {
 }
 
 export const ProjectPager = ({ page, prevPage, nextPage, page_count, total }: Props) => {
-    return (
-        <Pager>
-            <Pager.Item disabled={page === 0} onClick={prevPage}>
-                &larr; Previous
+    if (page_count === total) {
+        return <></>;
+    } else {
+        return (
+            <Pager>
+                <Pager.Item
+                    disabled={page === 0}
+                    onClick={prevPage}
+                    title="Previous"
+                >
+                    &larr; Previous
             </Pager.Item>
-            <li> {page * PAGE_SIZE + 1} - {(page * PAGE_SIZE) + page_count} of {total} </li>
-            <Pager.Item disabled={(page * PAGE_SIZE) + page_count >= total} onClick={nextPage}>
-                Next &rarr;
+                <li> {page * PAGE_SIZE + 1} - {(page * PAGE_SIZE) + page_count} of {total} </li>
+                <Pager.Item
+                    disabled={(page * PAGE_SIZE) + page_count >= total}
+                    onClick={nextPage}
+                    title="Next"
+                >
+                    Next &rarr;
             </Pager.Item>
-        </Pager>
-    );
+            </Pager>
+        );
+    }
 }
