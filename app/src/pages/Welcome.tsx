13e365e060e8605d3ab4ad29f1b7b063f7af6c0b
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2020-07-09 17:53:26 +0200

    Solve sonar analysis warnings

--- app/src/pages/Welcome.tsx
+++ app/src/pages/Welcome.tsx
@@ -72,7 +72,7 @@ export function Welcome() {
             </Row>
             <Row style={rowStyle}>
                 <Col md={4} mdOffset={2} style={colStyle}>
-                    <h1>Gene Cluster – Molecule Linking
+                    <h1>Gene Cluster - Molecule Linking
                 </h1>
                     <p style={textStyle}>
                         The Paired Omics Data Platform also collects publically available links between known biosynthetic gene clusters and molecules or molecular families. These fine-grained connections will inform future large scale predictions of structures from genomic data.
