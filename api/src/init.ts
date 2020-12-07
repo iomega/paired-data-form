021ee4e0662000f8d615c24dbec0b20eae6af150
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2020-12-07 14:40:40 +0100

    Dont load example env
    Its already loaded in `api/src/util/secrets.ts` in a smarter way

--- api/src/init.ts
+++ api/src/init.ts
@@ -1,11 +1,6 @@
-import dotenv from 'dotenv';
-
 import { DATADIR, REDIS_URL, ZENODO_UPLOAD_ENABLED, ELASTICSEARCH_URL } from './util/secrets';
 import { ProjectDocumentStore } from './projectdocumentstore';
 import { buildEnrichQueue, scheduledZenodoUploads } from './tasks';
-
-// Load environment variables from .env file, where API keys and passwords are configured
-dotenv.config({ path: '.env.example' });
 
 export const store = new ProjectDocumentStore(DATADIR, REDIS_URL, ELASTICSEARCH_URL);
 export const enrichqueue = buildEnrichQueue(store);
