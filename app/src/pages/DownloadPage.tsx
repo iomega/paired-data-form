96e4ae2ea0ffffd9edbba16fa6d330e224fa74c3
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2021-02-16 09:56:12 +0100

    Added link to Nature publication

--- app/src/pages/DownloadPage.tsx
+++ app/src/pages/DownloadPage.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { doiLink, doi } from "../constants";
+import { doiLink } from "../constants";
 
 const style = { padding: "10px" };
 
@@ -25,46 +25,6 @@ export const DownloadPage = () => {
         </b>{" "}
         Zenodo upload page.
       </div>
-      <h3>How to cite</h3>
-      <ul>
-        <li>
-          Please cite{" "}
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://doi.org/10.1038/s41589-020-00724-z"
-          >
-            10.1038/s41589-020-00724-z
-          </a>{" "}
-          for the Nature Chemical Biology paper
-        </li>
-        <li>
-          Please cite{" "}
-          <a target="_blank" rel="noopener noreferrer" href={doiLink}>
-            {doi}
-          </a>{" "}
-          if you use the projects dataset.
-        </li>
-        <li>
-          Please cite{" "}
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://doi.org/10.5281/zenodo.2656630"
-          >
-            10.5281/zenodo.2656630
-          </a>{" "}
-          if you use the software like this site or the{" "}
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://github.com/iomega/paired-data-form/blob/master/manuals/developers.md#web-service"
-          >
-            web service
-          </a>
-          .
-        </li>
-      </ul>
     </div>
   );
 };
