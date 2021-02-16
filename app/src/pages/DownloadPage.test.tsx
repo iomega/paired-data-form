f06371c2abf35bfce7234cab16672c17b1dc2cd0
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2021-02-16 10:02:32 +0100

    Move doi test to about page

--- app/src/pages/DownloadPage.test.tsx
+++ app/src/pages/DownloadPage.test.tsx
@@ -1,23 +1,21 @@
-import * as React from 'react';
+import * as React from "react";
 
-import { render, RenderResult } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
+import { render, RenderResult } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
 
-import { DownloadPage } from './DownloadPage';
+import { DownloadPage } from "./DownloadPage";
 
-describe('<DownloadPage/>', () => {
-    let wrapper: RenderResult;
+describe("<DownloadPage/>", () => {
+  let wrapper: RenderResult;
 
-    beforeEach(() => {
-        wrapper = render(<DownloadPage />);
-    });
+  beforeEach(() => {
+    wrapper = render(<DownloadPage />);
+  });
 
-    it('should render doi of dataset', () => {
-        expect(wrapper.baseElement).toHaveTextContent('10.5281/zenodo.3736430');
-    });
-
-    it('should render zenodo url of dataset', () => {
-        const a = wrapper.getByTitle('Download');
-        expect(a.getAttribute('href')).toEqual('https://doi.org/10.5281/zenodo.3736430');
-    });
+  it("should render zenodo url of dataset", () => {
+    const a = wrapper.getByTitle("Download");
+    expect(a.getAttribute("href")).toEqual(
+      "https://doi.org/10.5281/zenodo.3736430"
+    );
+  });
 });
