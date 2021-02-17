109f4ee3c05f08915956536243cc9fe6336adb69
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2021-02-17 09:25:39 +0100

    Mention project and dataset is CC BY 4.0 licensed

--- app/src/ProjectForm.tsx
+++ app/src/ProjectForm.tsx
@@ -1,6 +1,12 @@
 import * as React from "react";
 
-import { Button, ButtonGroup, ButtonToolbar, Glyphicon, Alert } from "react-bootstrap";
+import {
+  Button,
+  ButtonGroup,
+  ButtonToolbar,
+  Glyphicon,
+  Alert
+} from "react-bootstrap";
 import Form, { ISubmitEvent } from "react-jsonschema-form";
 import { useState, useRef } from "react";
 import CollapsibleField from "react-jsonschema-form-extras/lib/CollapsibleField";
@@ -9,7 +15,7 @@ import Ajv from "ajv";
 import { useSchema, useUiSchema } from "./api";
 import { ForeignKeyField } from "./fields/ForeignKeyField";
 import { HtmlDescriptionField } from "./fields/HtmlDescriptionField";
-import { TableField } from './fields/TableField';
+import { TableField } from "./fields/TableField";
 import { GenomeMetabolomeLinksField } from "./fields/GenomeMetabolomeLinksField";
 import { MyTitleField } from "./fields/TitleField";
 import { PairedDataProject } from "./PairedDataProject";
@@ -18,178 +24,200 @@ import { jsonDocument } from "./textTable";
 import { injectForeignKeySearchMethods, validateDocument } from "./validate";
 import { CheckList } from "./CheckList";
 
-import './ProjectForm.css';
+import "./ProjectForm.css";
 
 export interface IProps {
-    onSubmit(project: IOMEGAPairedOmicsDataPlatform): void;
-    formData?: IOMEGAPairedOmicsDataPlatform;
+  onSubmit(project: IOMEGAPairedOmicsDataPlatform): void;
+  formData?: IOMEGAPairedOmicsDataPlatform;
 }
 
 const formFields = {
-    collapsible: CollapsibleField,
-    foreignKey: ForeignKeyField,
-    gmtable: GenomeMetabolomeLinksField,
-    table: TableField,
-    TitleField: MyTitleField,
-    DescriptionField: HtmlDescriptionField
+  collapsible: CollapsibleField,
+  foreignKey: ForeignKeyField,
+  gmtable: GenomeMetabolomeLinksField,
+  table: TableField,
+  TitleField: MyTitleField,
+  DescriptionField: HtmlDescriptionField
 };
 
 export function ProjectForm({ onSubmit, formData }: IProps) {
-    const schema = useSchema();
-    const uiSchema = useUiSchema();
-    const [initDoc, setInitDoc] = useState(formData ? formData : undefined);
-    const [validDoc, setValidDoc] = useState<IOMEGAPairedOmicsDataPlatform | undefined>(undefined);
-    const [uploadError, setUploadError] = useState(false);
-    const onReset = () => {
-        setInitDoc(undefined);
+  const schema = useSchema();
+  const uiSchema = useUiSchema();
+  const [initDoc, setInitDoc] = useState(formData ? formData : undefined);
+  const [validDoc, setValidDoc] = useState<
+    IOMEGAPairedOmicsDataPlatform | undefined
+  >(undefined);
+  const [uploadError, setUploadError] = useState(false);
+  const onReset = () => {
+    setInitDoc(undefined);
+    setValidDoc(undefined);
+  };
+  const onError = () => {
+    setValidDoc(undefined);
+  };
+  const uploadRef = useRef<HTMLInputElement>(null);
+  const onUpload = () => {
+    uploadRef.current!.click();
+  };
+  const loadExample1 = () => {
+    if (uploadError) {
+      setUploadError(false);
+    }
+    fetch("/examples/paired_datarecord_MSV000078839_example.json")
+      .then(r => r.json())
+      .then(r => {
+        setInitDoc(r);
         setValidDoc(undefined);
+      });
+  };
+  const fillFormFromFile = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (!event.target.files) {
+      return;
     }
-    const onError = () => {
-        setValidDoc(undefined);
-    };
-    const uploadRef = useRef<HTMLInputElement>(null);
-    const onUpload = () => {
-        uploadRef.current!.click();
-    };
-    const loadExample1 = () => {
-        if (uploadError) {
-            setUploadError(false);
+    setUploadError(false);
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.onload = () => {
+      if (reader.result) {
+        try {
+          const project: IOMEGAPairedOmicsDataPlatform = JSON.parse(
+            reader.result as string
+          );
+          const ajv = new Ajv();
+          const valid = ajv.validate(schema.data as object, project);
+          if (valid) {
+            setInitDoc(project);
+            setValidDoc(undefined);
+          } else {
+            console.warn(ajv.errorsText());
+            setUploadError(true);
+          }
+        } catch (error) {
+          console.warn(error);
+          setUploadError(true);
         }
-        fetch('/examples/paired_datarecord_MSV000078839_example.json')
-            .then(r => r.json())
-            .then((r) => {
-                setInitDoc(r);
-                setValidDoc(undefined);
-            })
-            ;
-    }
-    const fillFormFromFile = (event: React.ChangeEvent<HTMLInputElement>) => {
-        if (!event.target.files) {
-            return;
-        }
-        setUploadError(false);
-        const file = event.target.files[0];
-        const reader = new FileReader();
-        reader.onload = () => {
-            if (reader.result) {
-                try {
-                    const project: IOMEGAPairedOmicsDataPlatform = JSON.parse(reader.result as string);
-                    const ajv = new Ajv();
-                    const valid = ajv.validate(schema.data as object, project);
-                    if (valid) {
-                        setInitDoc(project);
-                        setValidDoc(undefined);
-                    } else {
-                        console.warn(ajv.errorsText());
-                        setUploadError(true);
-                    }
-                } catch (error) {
-                    console.warn(error);
-                    setUploadError(true);
-                }
-            }
-        };
-        reader.readAsText(file);
-    }
-    const onWrappedSubmit = (event: ISubmitEvent<IOMEGAPairedOmicsDataPlatform>) => {
-        onSubmit(event.formData);
-    }
-    const formRef = useRef<Form<IOMEGAPairedOmicsDataPlatform>>(null);
-    if (uiSchema.data) {
-        injectForeignKeySearchMethods(uiSchema.data, formRef);
-    }
-    const onPreview = () => {
-        const theform: any = formRef.current;
-        // The form can not be validated (excluding submit) using it's public API
-        // Duplicate code from https://github.com/mozilla-services/react-jsonschema-form/blob/master/src/components/Form.js#L166-L167
-        const { errors, errorSchema } = theform.validate(theform.state.formData);
-        if (Object.keys(errors).length === 0) {
-            theform.setState({ errors, errorSchema }, () => {
-                setInitDoc(theform.state.formData);
-                setValidDoc(theform.state.formData);
-            });
-        } else {
-            theform.setState({ errors, errorSchema }, () => {
-                setInitDoc(theform.state.formData);
-                onError();
-            });
-        }
-    }
-    const uploadGenomeMetabolomeLinks = (gmrows: any[]) => {
-        const theform: any = formRef.current;
-        const currentData: any = { ...theform.state.formData };
-        const genome_metabolome_links = jsonDocument(currentData, gmrows);
-        currentData.genome_metabolome_links = genome_metabolome_links;
-        setInitDoc(currentData);
+      }
     };
-    const formContext = {
-        uploadGenomeMetabolomeLinks
-    };
-    if (schema.data && uiSchema.data) {
-        return (
-            <div className="projectform">
-                <CheckList />
-                <Form
-                    schema={schema.data}
-                    uiSchema={uiSchema.data}
-                    formData={initDoc ? initDoc : undefined}
-                    fields={formFields}
-                    validate={validateDocument}
-                    onSubmit={onWrappedSubmit}
-                    onError={onError}
-                    formContext={formContext}
-                    ref={formRef}
-                >
-                    {uploadError && (
-                        <Alert bsStyle="danger">Failed to parse JSON document. Try to validate the JSON document with an <a href="https://jsonschemalint.com/">online tool</a> against the <a target="_blank" href="/public/schema.json">JSON schema</a>.</Alert>
-                    )}
-                    <ButtonToolbar>
-                        <ButtonGroup>
-                            <Button onClick={loadExample1} title="Load example dataset">
-                                <Glyphicon glyph="paperclip" /> Example
-                            </Button>
-                            <Button onClick={onUpload} title="Upload JSON file">
-                                <Glyphicon glyph="upload" /> Upload
-                                <input
-                                    type="file"
-                                    accept="application/json,.json"
-                                    onChange={fillFormFromFile}
-                                    ref={uploadRef}
-                                    style={{ display: "none" }}
-                                />
-                            </Button>
-                        </ButtonGroup>
-                        <ButtonGroup>
-                            <Button type="submit" bsStyle="primary">
-                                <Glyphicon glyph="ok" /> Submit for review
-                            </Button>
-                            <Button bsStyle="info" onClick={onPreview}>
-                                <Glyphicon glyph="eye-open" /> Preview
-                            </Button>
-                            <Button type="reset" onClick={onReset}>
-                                <Glyphicon glyph="remove" /> Reset
-                            </Button>
-                        </ButtonGroup>
-                    </ButtonToolbar>
-                </Form>
-                {validDoc && (
-                    <>
-                        <h3>Preview</h3>
-                        <PairedDataProject
-                            project={{ _id: 'preview_id', project: validDoc }}
-                            schema={schema.data}
-                        />
-                    </>
-                )}
-            </div>
-        );
+    reader.readAsText(file);
+  };
+  const onWrappedSubmit = (
+    event: ISubmitEvent<IOMEGAPairedOmicsDataPlatform>
+  ) => {
+    onSubmit(event.formData);
+  };
+  const formRef = useRef<Form<IOMEGAPairedOmicsDataPlatform>>(null);
+  if (uiSchema.data) {
+    injectForeignKeySearchMethods(uiSchema.data, formRef);
+  }
+  const onPreview = () => {
+    const theform: any = formRef.current;
+    // The form can not be validated (excluding submit) using it's public API
+    // Duplicate code from https://github.com/mozilla-services/react-jsonschema-form/blob/master/src/components/Form.js#L166-L167
+    const { errors, errorSchema } = theform.validate(theform.state.formData);
+    if (Object.keys(errors).length === 0) {
+      theform.setState({ errors, errorSchema }, () => {
+        setInitDoc(theform.state.formData);
+        setValidDoc(theform.state.formData);
+      });
     } else {
-        if (schema.error) {
-            return <div>Error loading JSON schema: {schema.error.message}</div>;
-        }
-        if (uiSchema.error) {
-            return <div>Error loading UI schema: {uiSchema.error.message}</div>;
-        }
-        return <span>Loading ...</span>;
+      theform.setState({ errors, errorSchema }, () => {
+        setInitDoc(theform.state.formData);
+        onError();
+      });
     }
+  };
+  const uploadGenomeMetabolomeLinks = (gmrows: any[]) => {
+    const theform: any = formRef.current;
+    const currentData: any = { ...theform.state.formData };
+    const genome_metabolome_links = jsonDocument(currentData, gmrows);
+    currentData.genome_metabolome_links = genome_metabolome_links;
+    setInitDoc(currentData);
+  };
+  const formContext = {
+    uploadGenomeMetabolomeLinks
+  };
+  if (schema.data && uiSchema.data) {
+    return (
+      <div className="projectform">
+        <CheckList />
+        <Form
+          schema={schema.data}
+          uiSchema={uiSchema.data}
+          formData={initDoc ? initDoc : undefined}
+          fields={formFields}
+          validate={validateDocument}
+          onSubmit={onWrappedSubmit}
+          onError={onError}
+          formContext={formContext}
+          ref={formRef}
+        >
+          {uploadError && (
+            <Alert bsStyle="danger">
+              Failed to parse JSON document. Try to validate the JSON document
+              with an <a href="https://jsonschemalint.com/">online tool</a>{" "}
+              against the{" "}
+              <a target="_blank" href="/public/schema.json">
+                JSON schema
+              </a>
+              .
+            </Alert>
+          )}
+          <div>
+            By submitting your project to the platform, you agree that the
+            project is licensed under{" "}
+            <a href="https://creativecommons.org/licenses/by/4.0/">
+              Creative Commons Attribution 4.0 International
+            </a>{" "}
+            and can be distributed as part of the platforms{" "}
+            <a href="https://doi.org/10.5281/zenodo.3736430">dataset</a>.
+          </div>
+          <ButtonToolbar>
+            <ButtonGroup>
+              <Button onClick={loadExample1} title="Load example dataset">
+                <Glyphicon glyph="paperclip" /> Example
+              </Button>
+              <Button onClick={onUpload} title="Upload JSON file">
+                <Glyphicon glyph="upload" /> Upload
+                <input
+                  type="file"
+                  accept="application/json,.json"
+                  onChange={fillFormFromFile}
+                  ref={uploadRef}
+                  style={{ display: "none" }}
+                />
+              </Button>
+            </ButtonGroup>
+            <ButtonGroup>
+              <Button type="submit" bsStyle="primary">
+                <Glyphicon glyph="ok" /> Submit for review
+              </Button>
+              <Button bsStyle="info" onClick={onPreview}>
+                <Glyphicon glyph="eye-open" /> Preview
+              </Button>
+              <Button type="reset" onClick={onReset}>
+                <Glyphicon glyph="remove" /> Reset
+              </Button>
+            </ButtonGroup>
+          </ButtonToolbar>
+        </Form>
+        {validDoc && (
+          <>
+            <h3>Preview</h3>
+            <PairedDataProject
+              project={{ _id: "preview_id", project: validDoc }}
+              schema={schema.data}
+            />
+          </>
+        )}
+      </div>
+    );
+  } else {
+    if (schema.error) {
+      return <div>Error loading JSON schema: {schema.error.message}</div>;
+    }
+    if (uiSchema.error) {
+      return <div>Error loading UI schema: {uiSchema.error.message}</div>;
+    }
+    return <span>Loading ...</span>;
+  }
 }
--- app/src/pages/DownloadPage.tsx
+++ app/src/pages/DownloadPage.tsx
@@ -25,6 +25,20 @@ export const DownloadPage = () => {
         </b>{" "}
         Zenodo upload page.
       </div>
+      <br />
+      <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">
+        <img
+          alt="Creative Commons License"
+          style={{ borderWidth: 0 }}
+          src="https://i.creativecommons.org/l/by/4.0/88x31.png"
+        />
+      </a>
+      <br />
+      This work is licensed under a{" "}
+      <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">
+        Creative Commons Attribution 4.0 International License
+      </a>
+      .
     </div>
   );
 };
