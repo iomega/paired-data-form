51ad5095889972569b1540bd08e8a3507e24db71
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2021-04-26 11:34:14 +0200

    Implement migration from v2 to v3 json schema

--- api/src/migrate.test.ts
+++ api/src/migrate.test.ts
@@ -1,4 +1,3 @@
-import { IOMEGAPairedOmicsDataPlatform } from './schema';
 import { migrations, Migration, migrate } from './migrate';
 
 const EXAMPLE_PROJECT_JSON_V1 = {
@@ -237,6 +236,131 @@ const EXAMPLE_PROJECT_JSON_V2 = {
     ]
 };
 
+const EXAMPLE_PROJECT_JSON_V3: any = {
+    'version': '3',
+    'personal': {
+        'submitter_name': 'Justin van der Hooft',
+        'submitter_orcid': 'https://orcid.org/0000-0002-9340-5511',
+        'submitter_email': 'justin.vanderhooft@wur.nl',
+        'PI_name': 'Marnix Medema',
+        'PI_institution': 'Wageningen University & Research',
+        'PI_email': 'marnix.medema@wur.nl'
+    },
+    'metabolomics': {
+        'project': {
+            'GNPSMassIVE_ID': 'MSV000078839',
+            'MaSSIVE_URL': 'https://massive.ucsd.edu/ProteoSAFe/dataset.jsp?task=a507232a787243a5afd69a6c6fa1e508',
+            'molecular_network': 'c36f90ba29fe44c18e96db802de0c6b9'
+        },
+        'related_GNPSMassIVE_ID': 'MSV000078836',
+        'publications': '28335604'
+    },
+    'genomes': [
+        {
+            'genome_ID': {
+                'genome_type': 'genome',
+                'GenBank_accession': 'AZXI01000001'
+            },
+            'BioSample_accession': 'SAMN02597265',
+            'publications': '28335604',
+            'genome_label': 'Salinispora arenicola CNB527'
+        }
+    ],
+    'proteomes': [],
+    'experimental': {
+        'sample_preparation': [
+            {
+                'medium_details': {
+                    'medium_type': 'solid',
+                    'medium': 'https://www.elabprotocols.com/protocols/#!protocol=486'
+                },
+                'growth_parameters': {
+                    'growth_duration': 168,
+                    'growth_temperature': 30
+                },
+                'aeration': {
+                    'aeration_type': 'not shaking'
+                },
+                'sample_preparation_method': 'R5'
+            }
+        ],
+        'extraction_methods': [
+            {
+                'solvents': [
+                    {
+                        'solvent': 'http://purl.obolibrary.org/obo/CHEBI_27750',
+                        'ratio': 1
+                    }
+                ],
+                'extracted_material': 'cells',
+                'extraction_method': 'EthylAcetate'
+            }
+        ],
+        'instrumentation_methods': [
+            {
+                'instrumentation': {
+                    'instrument': 'http://purl.obolibrary.org/obo/MS_1000084'
+                },
+                'column': 'Reverse Phase',
+                'mode': 'http://purl.obolibrary.org/obo/MS_1000130',
+                'ionization': {
+                    'ionization_type': 'http://purl.obolibrary.org/obo/MS_1000073'
+                },
+                'range': '300 - 1700',
+                'collision_energy': '20 keV',
+                'buffering': '0.1% TFA',
+                'instrumentation_method': 'ToF'
+            }
+        ]
+    },
+    'genome_metabolome_links': [
+        {
+            'genome_label': 'Salinispora arenicola CNB527',
+            'metabolomics_file': 'ftp://massive.ucsd.edu/MSV000078839/spectrum/R5/CNB527_R5_E.mzXML',
+            'sample_preparation_label': 'R5',
+            'extraction_method_label': 'EthylAcetate',
+            'instrumentation_method_label': 'ToF'
+        }
+    ],
+    'BGC_MS2_links': [
+        {
+            'known_link': 'A Molecular Family of Staurosporine and Hydroxystaurosporine and related molecules that can be linked to a reference gene cluster in MiBIG from Salinispora CNS205 that is included in this study in which it was earlier validated.',
+            'verification': [
+                'Evidence as indicated in MIBiG'
+            ],
+            'quantitative_experiment': {
+                'quantitative_experiment_type': 'Not available'
+            },
+            'SMILES': 'C[C@@]12[C@@H]([C@@H](C[C@@H](O1)N3C4=CC=CC=C4C5=C6C(=C7C8=CC=CC=C8N2C7=C53)CNC6=O)NC)OC',
+            'IUPAC': '(2S,3R,4R,6R)-3-methoxy-2-methyl-4-(methylamino)-29-oxa-1,7,17-triazaoctacyclo[12.12.2.12,6.07,28.08,13.015,19.020,27.021,26]nonacosa-8,10,12,14,19,21,23,25,27-nonaen-16-one',
+            'BGC_ID': {
+                'BGC': 'MIBiG number associated with this exact BGC',
+                'MIBiG_number': 'BGC0000827'
+            },
+            'link': 'GNPS molecular family',
+            'network_nodes_URL': 'https://gnps.ucsd.edu/ProteoSAFe/result.jsp?view=network_displayer&componentindex=201&task=c36f90ba29fe44c18e96db802de0c6b9#%7B%7D'
+        },
+        {
+            'known_link': 'Arenimycin A as produced by Salinispora strain CNB527 which is present under the strains studied. It was only extracted by EthylAcetate.',
+            'verification': [
+                'Evidence as indicated in MIBiG'
+            ],
+            'quantitative_experiment': {
+                'quantitative_experiment_type': 'Not available'
+            },
+            'SMILES': 'CCCOC(=O)c1c(C)cc2c(c1O)[C@@]1(O)C(=O)c3cc4c(c(O)c3C(=O)[C@@]1(OC)CC2)C(=O)C=C(N[C@H]1O[C@@H](C)[C@H](O)[C@H](O)[C@H]1OC)C4=O',
+            'IUPAC': 'methyl (6aR,14aS)-11-[[(2S,3R,4R,5R,6S)-4,5-dihydroxy-3-methoxy-6-methyloxan-2-yl]amino]-1,8,14a-trihydroxy-6a-methoxy-3-methyl-7,9,12,14-tetraoxo-5,6-dihydrobenzo[a]tetracene-2-carboxylate',
+            'BGC_ID': {
+                'BGC': 'MIBiG number of a similar BGC from a closely related strain',
+                'similar_MIBiG_number': 'BGC0000198'
+            },
+            'link': 'single molecule',
+            'MS2_URL': 'ftp://massive.ucsd.edu/MSV000078839/spectrum/R5/CNB527_R5_E.mzXML',
+            'MS2_scan': '977106'
+        }
+    ]
+};
+
 describe('migrateProject1to2 migration ', () => {
     let migration: Migration;
 
@@ -246,7 +370,7 @@ describe('migrateProject1to2 migration ', () => {
 
     describe('up()', () => {
         describe('with version 1 project', () => {
-            let result: IOMEGAPairedOmicsDataPlatform;
+            let result: any;
 
             beforeAll(() => {
                 // migration happens in place, to compare later need a untouched example
@@ -270,6 +394,10 @@ describe('migrateProject1to2 migration ', () => {
         describe('with version 1 project', () => {
             it('should migrate', () => {
                 const result = migration.applicable(EXAMPLE_PROJECT_JSON_V1);
+                console.log(
+                    migration.applicable,
+                    EXAMPLE_PROJECT_JSON_V1.version
+                    );
                 expect(result).toBeTruthy();
             });
         });
@@ -285,10 +413,80 @@ describe('migrateProject1to2 migration ', () => {
 
 });
 
+describe('migrateProject2to3 migration ', () => {
+    let migration: Migration;
+
+    beforeAll(() => {
+        migration = migrations[1];
+    });
+
+    describe('up()', () => {
+        describe('with version 2 project', () => {
+            let result: any;
+
+            beforeAll(() => {
+                // migration happens in place, to compare later need a untouched example
+                const copy = JSON.parse(JSON.stringify(EXAMPLE_PROJECT_JSON_V2));
+                //
+                copy.experimental.instrumentation_methods[0].column = 'HILIC';
+                result = migration.up(copy);
+            });
+
+            it('should have version to 3', () => {
+                expect(result.version).toEqual('3');
+            });
+
+            it('should have HILIC spelled out', () => {
+                expect(result.experimental.instrumentation_methods[0].column).toEqual('Hydrophilic interaction (HILIC)');
+            });
+
+            it('should have empty proteomes', () => {
+                expect(result.proteomes).toEqual([]);
+            });
+
+            it('should have not available quantitive experiment type', () => {
+                expect.assertions(2);
+                result.BGC_MS2_links.forEach((l: any) => {
+                    expect(l.quantitative_experiment).toEqual({
+                        quantitative_experiment_type: 'Not available'
+                    });
+                });
+            });
+        });
+    });
+
+    describe('applicable()', () => {
+        describe('with version 2 project', () => {
+            it('should migrate', () => {
+                const result = migration.applicable(EXAMPLE_PROJECT_JSON_V2);
+                console.log(
+                    migration.applicable,
+                    EXAMPLE_PROJECT_JSON_V2.version
+                    );
+                expect(result).toBeTruthy();
+            });
+        });
+
+        describe('with version 1 project', () => {
+            it('should not migrate', () => {
+                const result = migration.applicable(EXAMPLE_PROJECT_JSON_V1);
+                expect(result).toBeFalsy();
+            });
+        });
+        describe('with version 3 project', () => {
+            it('should not migrate', () => {
+                const result = migration.applicable(EXAMPLE_PROJECT_JSON_V3);
+                expect(result).toBeFalsy();
+            });
+        });
+    });
+
+});
+
 describe('migrate()', () => {
     let store: any;
 
-    describe('with 2 approved and 2 pending project of version 1 and 2', () => {
+    describe('with 3 approved and 3 pending project of version 1, 2 and 3', () => {
         beforeAll(() => {
             store = {
                 listPendingProjects() {
@@ -298,6 +496,9 @@ describe('migrate()', () => {
                     }, {
                         _id: 'somependingprojectid2',
                         project: JSON.parse(JSON.stringify(EXAMPLE_PROJECT_JSON_V2))
+                    }, {
+                        _id: 'somependingprojectid3',
+                        project: JSON.parse(JSON.stringify(EXAMPLE_PROJECT_JSON_V3))
                     }];
                 },
                 listProjects() {
@@ -307,6 +508,9 @@ describe('migrate()', () => {
                     }, {
                         _id: 'someapprovedprojectid2',
                         project: JSON.parse(JSON.stringify(EXAMPLE_PROJECT_JSON_V2))
+                    }, {
+                        _id: 'someapprovedprojectid3',
+                        project: JSON.parse(JSON.stringify(EXAMPLE_PROJECT_JSON_V3))
                     }];
                 },
                 disk_store: {
@@ -318,20 +522,23 @@ describe('migrate()', () => {
         });
 
         it('should written migrated pending project', () => {
-            expect(store.disk_store.writePendingProject).toHaveBeenCalledWith('somependingprojectid1', EXAMPLE_PROJECT_JSON_V2);
+            expect(store.disk_store.writePendingProject).toHaveBeenCalledWith('somependingprojectid1', EXAMPLE_PROJECT_JSON_V3);
+            expect(store.disk_store.writePendingProject).toHaveBeenCalledWith('somependingprojectid2', EXAMPLE_PROJECT_JSON_V3);
         });
 
         it('should written migrated prevously approved as pending project', () => {
-            expect(store.disk_store.writePendingProject).toHaveBeenCalledWith('someapprovedprojectid1', EXAMPLE_PROJECT_JSON_V2);
+            expect(store.disk_store.writePendingProject).toHaveBeenCalledWith('someapprovedprojectid1', EXAMPLE_PROJECT_JSON_V3);
+            expect(store.disk_store.writePendingProject).toHaveBeenCalledWith('someapprovedprojectid2', EXAMPLE_PROJECT_JSON_V3);
         });
 
         it('should have approved the prevously approved project', () => {
             expect(store.disk_store.approveProject).toHaveBeenCalledWith('someapprovedprojectid1');
+            expect(store.disk_store.approveProject).toHaveBeenCalledWith('someapprovedprojectid2');
         });
 
-        it('should not have written projects that where already of version 2', () => {
-            expect(store.disk_store.writePendingProject).toHaveBeenCalledTimes(2);
-            expect(store.disk_store.approveProject).toHaveBeenCalledTimes(1);
+        it('should not have written projects that where already of version 3', () => {
+            expect(store.disk_store.writePendingProject).toHaveBeenCalledTimes(4);
+            expect(store.disk_store.approveProject).toHaveBeenCalledTimes(2);
         });
     });
 });
--- api/src/migrate.ts
+++ api/src/migrate.ts
@@ -1,31 +1,27 @@
-import { IOMEGAPairedOmicsDataPlatform } from './schema';
 import { ProjectDocumentStore } from './projectdocumentstore';
-import { loadSchema } from './util/schema';
-
-const schema = loadSchema();
-const schemaVersion = schema.properties.version.default;
 
 export interface Migration {
     applicable(p: any): boolean;
-    up(p: any): IOMEGAPairedOmicsDataPlatform;
-}
-
-function bgc2string(value: string | number) {
-    if (value.toString().startsWith('BGC')) {
-        return value;
-    }
-    return 'BGC' + value.toString().padStart(7, '0');
+    up(p: any): any;
 }
 
 export const migrations: Migration[] = [
     {
-        applicable: (p: any) => p.version === '1' && schemaVersion === '2',
+        applicable: (p: any) => p.version === '1',
         up: (project: any) => {
             // bump version
             project.version = '2';
 
             // MIBiG accession instead of number
             if (project.BGC_MS2_links) {
+
+                function bgc2string(value: string | number) {
+                    if (value.toString().startsWith('BGC')) {
+                        return value;
+                    }
+                    return 'BGC' + value.toString().padStart(7, '0');
+                }
+
                 project.BGC_MS2_links.forEach((r: any) => {
                     if (r.BGC_ID.BGC === 'MIBiG number associated with this exact BGC') {
                         r.BGC_ID.MIBiG_number = bgc2string(r.BGC_ID.MIBiG_number);
@@ -39,6 +35,40 @@ export const migrations: Migration[] = [
 
             return project;
         }
+    },
+    {
+        applicable: (p: any) => p.version === '2',
+        up: (project: any) => {
+            // bump version
+            project.version = '3';
+
+            // column HILIC renamed to `Hydrophilic interaction (HILIC)`
+            if (project.experimental.instrumentation_methods) {
+                project.experimental.instrumentation_methods.forEach((r: any) => {
+                    if (r.column === 'HILIC') {
+                        r.column = 'Hydrophilic interaction (HILIC)';
+                    }
+                });
+            }
+
+            // Proteomes added
+            if (!project.proteomes) {
+                project.proteomes = [];
+            }
+
+            // quantitative_experiment added
+            if (project.BGC_MS2_links) {
+                project.BGC_MS2_links.forEach((r: any) => {
+                    if (!r.quantitative_experiment) {
+                        r.quantitative_experiment = {
+                            quantitative_experiment_type: 'Not available'
+                        }
+                    }
+                });
+            }
+
+            return project;
+        }
     }
 ];
 
@@ -46,24 +76,33 @@ export const migrate = async (store: ProjectDocumentStore) => {
     console.log('Migrating pending projects');
     const pending_projects = await store.listPendingProjects();
     for (const p of pending_projects) {
+        let migrated = false;
         for (const m of migrations) {
             if (m.applicable(p.project)) {
-                console.log(`Project ${p._id}`);
-                const new_project = m.up(p.project);
-                await store.disk_store.writePendingProject(p._id, new_project);
+                console.log(`Project ${p._id} v${p.project.version}`);
+                p.project = m.up(p.project);
+                p.project = m.up(p.project);
+                migrated = true;
             }
+        }
+        if (migrated) {
+            await store.disk_store.writePendingProject(p._id, p.project);
         }
     }
     console.log('Migrating approved projects');
     const projects = await store.listProjects();
     for (const p of projects) {
+        let migrated = false;
         for (const m of migrations) {
             if (m.applicable(p.project)) {
-                console.log(`Project ${p._id}`);
-                const new_project = m.up(p.project);
-                await store.disk_store.writePendingProject(p._id, new_project);
-                await store.disk_store.approveProject(p._id);
+                console.log(`Project ${p._id} v${p.project.version}`);
+                p.project = m.up(p.project);
+                migrated = true;
             }
+        }
+        if (migrated) {
+            await store.disk_store.writePendingProject(p._id, p.project);
+            await store.disk_store.approveProject(p._id);
         }
     }
 };
